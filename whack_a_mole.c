e6f92f7531db7da2c7677e9ed209e8de26003f0a
Mark Gondree <gondree@gmail.com>
2023-04-20 23:06:53 -0700

    Added some new behaviors

--- whack_a_mole.c
+++ whack_a_mole.c
@@ -5,40 +5,70 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <openssl/sha.h>
+#include <signal.h>
+
+static unsigned char hash[50];
+static char *data = NULL;
 
 void useage(void)
 {
     ;
 }
 
-void shasum(const unsigned char* data)
-{
-  int i;
-  size_t length = strlen(data);
-  unsigned char hash[50];
 
-  SHA1(data, length-1, hash);
-  for(i = 0; i<SHA_DIGEST_LENGTH; i++){
-    printf("%x", hash[i]);
-  }
-  printf("\n");
+void secret()
+{
+    int i;
+    size_t length = strlen(data);
+
+    SHA1(data, length-1, hash);
+    for(i = 0; i<SHA_DIGEST_LENGTH; i++){
+        printf("%x", hash[i]);
+    }
+    printf("\n");
 }
 
-void handler_exit() {
-   printf("You got me to exit.\n");
+void handler_1() {
+    printf("You got me to exit.\n");
+    exit(0);
+}
+
+void handler_2() {
+    printf("Mathematical!\n");
+}
+
+void handler_3() {
+    printf("Algebraic!\n");
 }
 
 void handler_kill() {
-   printf("You have to kill -9 me.\n"); fflush(stdout);
+    printf("You have to kill -9 me.\n"); fflush(stdout);
 }
 
-void run(char *data)
+void run()
 {
-    atexit(handler_exit);
+    sigset_t mask;
+    sigfillset(&mask);
+
+    sigdelset(&mask, SIGINT);
     signal(SIGINT, handler_kill);
-    signal(SIGTERM, handler_kill);
-    signal(SIGHUP, handler_kill);
-    signal(SIGILL, exit);
+
+    sigdelset(&mask, SIGSTOP);
+    sigdelset(&mask, SIGCONT);
+
+    sigdelset(&mask, SIGTERM);
+    signal(SIGTERM, handler_1);
+
+    sigdelset(&mask, SIGHUP);
+    signal(SIGHUP, handler_2);
+
+    sigdelset(&mask, SIGPIPE);
+    signal(SIGPIPE, handler_3);
+
+    sigdelset(&mask, SIGUSR1);
+    signal(SIGUSR1, secret);
+
+    sigprocmask(SIG_SETMASK, &mask, NULL);
 
     while(1) {
         sleep(1);
@@ -50,41 +80,34 @@ void run(char *data)
 
 int main (int argc, char **argv)
 {
-   int c, opt_idx=0;
-   char *data = NULL;
-   static struct option long_options[] =
-   {
+    int c, opt_idx = 0;
+    static struct option long_options[] = {
         {"help",  no_argument,       0, 'h'},
-        {"data",  required_argument, 0, 'd'},
         {0, 0, 0, 0}
-   };
+    };
 
-   while (1)
-   {
-      c = getopt_long (argc, argv, "hd:", long_options, &opt_idx);
-      if (c == -1){  // end of the options
-          break;
-      }
+    while (1)
+    {
+        c = getopt_long (argc, argv, "h", long_options, &opt_idx);
+        if (c == -1){  // end of the options
+            break;
+        }
+        switch (c)
+        {
+            case 'h':
+              useage();
+              exit(0);
+              break;
+            default:
+              useage();
+              exit(1);
+            break;
+        }
+    }
 
-      switch (c)
-      {
-        case 'h':
-          useage();
-          exit(0);
-          break;
-        case 'd':
-          data = optarg;
-          break;
-        default:
-          useage();
-          exit(1);
-          break;
-      }
-   }
-   if (data == NULL){
-       data = argv[argc-1];
-   }
-   run(data);
-   exit (0);
+    data = argv[argc-1];
+    run();
+
+    exit(0);
 }
 
